e8242d1c01e488ca9c59343e2f2e3656ecaeba15
Janey Slinger <janeyslinger@gmail.com>
2019-12-26 20:48:54 +0000

    Cauldron and bubbles textures

--- maps/furniture.tsx
+++ maps/furniture.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.3.1" name="furniture" tilewidth="32" tileheight="32" tilecount="10" columns="5">
- <image source="furniture.png" trans="ffffff" width="160" height="64"/>
+<tileset version="1.2" tiledversion="1.3.1" name="furniture" tilewidth="32" tileheight="32" tilecount="27" columns="9">
+ <image source="furniture.png" trans="ffffff" width="288" height="96"/>
 </tileset>
