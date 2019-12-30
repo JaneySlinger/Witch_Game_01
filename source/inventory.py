12c41b403ce520138c50207d6d079ec7e6881030
Janey Slinger <janeyslinger@gmail.com>
2019-12-30 16:35:52 +0000

    Started work on quests

--- source/inventory.py
+++ source/inventory.py
@@ -1,5 +1,8 @@
 import arcade
 TILE_SCALING = 1
+SCREEN_WIDTH = 1024
+SCREEN_HEIGHT = 640
+NUMBER_OF_QUESTS = 3
 
 
 class InventoryView(arcade.View):
@@ -23,6 +26,7 @@ class InventoryView(arcade.View):
         target_list = arcade.tilemap.process_layer(
             inv_map, inventory_layer_name, TILE_SCALING)
 
+        # set items to dim if they haven't been collected and bright if they have been collected
         for item in target_list:
             item.intensity = 'dim'
             item.alpha = 64
@@ -33,10 +37,31 @@ class InventoryView(arcade.View):
                     item.intensity = 'bright'
                     item.alpha = 255
 
+        arcade.draw_text("QUESTS:", SCREEN_WIDTH / 2, SCREEN_HEIGHT - 50, arcade.color.BLACK, 24,
+                         align="left", anchor_x="left", anchor_y="center"
+                         )
+
+        # draw the icons for the quests
+        vertical = 100
+        for i in range(0, NUMBER_OF_QUESTS):
+            arcade.draw_text("-", SCREEN_WIDTH / 2, SCREEN_HEIGHT - vertical, arcade.color.BLACK, 20,
+                             align="left", anchor_x="left", anchor_y="center"
+                             )
+            vertical += 50
+
+        x_variable = (SCREEN_WIDTH / 2) + 50
+        arcade.draw_text("Find a potion recipe book", x_variable, SCREEN_HEIGHT - 100, arcade.color.BLACK, 20,
+                         align="left", anchor_x="left", anchor_y="center"
+                         )
+        arcade.draw_text("Find the ingredients for the potion", x_variable, SCREEN_HEIGHT - 150, arcade.color.BLACK, 20,
+                         align="left", anchor_x="left", anchor_y="center"
+                         )
+        arcade.draw_text("Add the ingredients to the cauldron", x_variable, SCREEN_HEIGHT - 200, arcade.color.BLACK, 20,
+                         align="left", anchor_x="left", anchor_y="center"
+                         )
+
         target_list.draw()
 
     def on_key_press(self, key, modifiers):
         if key == arcade.key.ESCAPE:
-            # how do I know which view to return it to. It could be the interior one.
-            # self.window.show_view(self.game_view)
             self.window.show_view(self.return_view)
